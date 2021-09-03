d7e333454b168fef160bf079b3490718f60eb7ee
Lucas Martins <lucas.pemartins@gmail.com>
2021-09-03 01:06:53 +0100

    Create constants on service

--- src/main/java/org/debreuck/neirynch/documentsmanager/service/DocumentManagerService.java
+++ src/main/java/org/debreuck/neirynch/documentsmanager/service/DocumentManagerService.java
@@ -20,6 +20,14 @@ import org.springframework.stereotype.Service;
 public class DocumentManagerService {
 	
 	private static final Logger LOG = Logger.getLogger(DocumentManagerService.class);
+	private static final int DATE_INITIAL_INDEX = 0;
+	private static final int DATE_FINAL_INDEX = 23;
+	private static final String RETURNED = "returned";
+	private static final String LEFT_BRACKET = "[";
+	private static final String RIGHT_BRACKET = "]";
+	private static final int START_RENDERING_LEFT_BRACKET_LINE_OCCURRENCE_NUMBER = 3;
+	private static final int START_RENDERING_RIGHT_BRACKET_LINE_OCCURRENCE_NUMBER = 3;
+	private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss.SSS";
 
 	public Report analyzeDocument(InputStream logFile) {
 		try (InputStream in = logFile; BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
@@ -31,16 +39,16 @@ public class DocumentManagerService {
 			Set<Rendering> rederingSet = new HashSet<>();
 			while ((line = reader.readLine()) != null) {
 				if (line.contains(LogMessage.START_RENDERING.getLogMessageContent())) {
-					start = line.substring(0, 23);
+					start = line.substring(DATE_INITIAL_INDEX, DATE_FINAL_INDEX);
 					start = start.replaceFirst(",", ".");
-					startTime = LocalDateTime.from(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").parse(start));
-					String startRenderingArguments = line.substring(StringUtils.ordinalIndexOf(line, "[", 3) + 1,
-							StringUtils.ordinalIndexOf(line, "]", 3));
+					startTime = LocalDateTime.from(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN).parse(start));
+					String startRenderingArguments = line.substring(StringUtils.ordinalIndexOf(line, LEFT_BRACKET, START_RENDERING_LEFT_BRACKET_LINE_OCCURRENCE_NUMBER) + 1,
+							StringUtils.ordinalIndexOf(line, RIGHT_BRACKET, START_RENDERING_RIGHT_BRACKET_LINE_OCCURRENCE_NUMBER));
 					String[] startRenderingArgumentsArray = startRenderingArguments.split(",");
 					documentId = startRenderingArgumentsArray[0].trim();
 					page = startRenderingArgumentsArray[1].trim();
 				} else if (line.contains(LogMessage.START_RENDERING_RETURN.getLogMessageContent())) {
-					int uidFirstIndex = line.indexOf("returned") + 8;
+					int uidFirstIndex = line.indexOf(RETURNED) + RETURNED.length();
 					String uid = line.substring(uidFirstIndex).trim();
 					Long document = Long.parseLong(documentId);
 					Long pageLong = Long.parseLong(page);
@@ -49,12 +57,12 @@ public class DocumentManagerService {
 					LocalDateTime newStartTime = startTime;
 					rederingSet.stream().filter(r -> r.getUid().equals(uid)).findFirst().ifPresent(r -> r.getStart().add(newStartTime));
 				} else if (line.contains(LogMessage.GET_RENDERING.getLogMessageContent())) {
-					String getTime = line.substring(0, 23);
+					String getTime = line.substring(DATE_INITIAL_INDEX, DATE_FINAL_INDEX);
 					getTime = getTime.replaceFirst(",", ".");
-					String getRenderingArguments = line.substring(StringUtils.ordinalIndexOf(line, "[", 3) + 1,
-							StringUtils.ordinalIndexOf(line, "]", 3));
+					String getRenderingArguments = line.substring(StringUtils.ordinalIndexOf(line, LEFT_BRACKET, START_RENDERING_LEFT_BRACKET_LINE_OCCURRENCE_NUMBER) + 1,
+							StringUtils.ordinalIndexOf(line, RIGHT_BRACKET, START_RENDERING_RIGHT_BRACKET_LINE_OCCURRENCE_NUMBER));
 					LocalDateTime dateTime = LocalDateTime
-							.from(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").parse(getTime));
+							.from(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN).parse(getTime));
 					rederingSet.stream().filter(r -> r.getUid().equals(getRenderingArguments)).findFirst().ifPresent(r -> r.getGet().add(dateTime));
 				}
 			}
